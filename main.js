12fd08177b89c199ecd318ba1933de9fe9423fc9
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2022-02-28 14:20:32 +0100

    mise à jour

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import http from 'http';
 import RequestController from './controllers/requestController.js';
 import { Server as IOServer } from 'socket.io';
 import IOController from './controllers/ioController.js';
+var PORT = process.env.PORT || 3000
 const server = http.createServer(
 	(request, response) => new RequestController(request, response).handleRequest()
 );
@@ -10,4 +11,4 @@ const io = new IOServer(server);
 const ioController = new IOController(io);
 
 io.on('connection', ioController.registerSocket.bind(ioController));
-server.listen(8080);
+server.listen(PORT);
